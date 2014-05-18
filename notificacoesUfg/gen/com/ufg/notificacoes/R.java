48ac6c9198f5eaea260541c6148f4677682a620d
Italo Gustavo <italogmm@gmail.com>
2014-05-18 14:15:52 -0300

    Tela de consulta de notificação.

--- notificacoesUfg/gen/com/ufg/notificacoes/R.java
+++ notificacoesUfg/gen/com/ufg/notificacoes/R.java
@@ -1088,14 +1088,16 @@ containing a value of this type.
         public static final int action_bar_subtitle=0x7f050023;
         public static final int action_bar_title=0x7f050022;
         public static final int action_context_bar=0x7f05001d;
-        public static final int action_forgot_password=0x7f050046;
+        public static final int action_forgot_password=0x7f05004a;
         public static final int action_menu_divider=0x7f050016;
         public static final int action_menu_presenter=0x7f050017;
         public static final int action_mode_close_button=0x7f050024;
-        public static final int action_settings=0x7f050045;
+        public static final int action_settings=0x7f050049;
         public static final int activity_chooser_view_content=0x7f050025;
         public static final int always=0x7f05000b;
         public static final int beginning=0x7f050011;
+        public static final int button1=0x7f050048;
+        public static final int button2=0x7f050047;
         public static final int checkbox=0x7f05002d;
         public static final int collapseActionView=0x7f05000d;
         public static final int container=0x7f05003c;
@@ -1128,6 +1130,7 @@ containing a value of this type.
         public static final int progress_circular=0x7f050018;
         public static final int progress_horizontal=0x7f050019;
         public static final int radio=0x7f05002f;
+        public static final int remetente=0x7f050045;
         public static final int search_badge=0x7f050032;
         public static final int search_bar=0x7f050031;
         public static final int search_button=0x7f050033;
@@ -1146,6 +1149,7 @@ containing a value of this type.
         public static final int split_action_bar=0x7f05001e;
         public static final int submit_area=0x7f050039;
         public static final int tabMode=0x7f050002;
+        public static final int textView1=0x7f050046;
         public static final int title=0x7f05002b;
         public static final int top_action_bar=0x7f050020;
         public static final int up=0x7f050021;
@@ -1202,17 +1206,20 @@ containing a value of this type.
         public static final int abc_search_dropdown_item_icons_2line=0x7f030015;
         public static final int abc_search_view=0x7f030016;
         public static final int abc_simple_decor=0x7f030017;
-        public static final int activity_inicio=0x7f030018;
-        public static final int activity_login=0x7f030019;
-        public static final int activity_main=0x7f03001a;
-        public static final int fragment_inicio=0x7f03001b;
-        public static final int fragment_main=0x7f03001c;
-        public static final int support_simple_spinner_dropdown_item=0x7f03001d;
+        public static final int activity_consulta_notificacao=0x7f030018;
+        public static final int activity_inicio=0x7f030019;
+        public static final int activity_login=0x7f03001a;
+        public static final int activity_main=0x7f03001b;
+        public static final int fragment_consulta_notificacao=0x7f03001c;
+        public static final int fragment_inicio=0x7f03001d;
+        public static final int fragment_main=0x7f03001e;
+        public static final int support_simple_spinner_dropdown_item=0x7f03001f;
     }
     public static final class menu {
-        public static final int inicio=0x7f0c0000;
-        public static final int login=0x7f0c0001;
-        public static final int main=0x7f0c0002;
+        public static final int consulta_notificacao=0x7f0c0000;
+        public static final int inicio=0x7f0c0001;
+        public static final int login=0x7f0c0002;
+        public static final int main=0x7f0c0003;
     }
     public static final class string {
         /**  Content description for the action bar "home" affordance. [CHAR LIMIT=NONE] 
@@ -1256,23 +1263,24 @@ containing a value of this type.
          */
         public static final int abc_shareactionprovider_share_with_application=0x7f0a000b;
         public static final int action_entrar=0x7f0a0010;
-        public static final int action_forgot_password=0x7f0a0017;
+        public static final int action_forgot_password=0x7f0a0018;
         public static final int action_settings=0x7f0a000f;
-        public static final int action_sign_in_register=0x7f0a0015;
-        public static final int action_sign_in_short=0x7f0a0016;
+        public static final int action_sign_in_register=0x7f0a0016;
+        public static final int action_sign_in_short=0x7f0a0017;
         public static final int app_name=0x7f0a000d;
-        public static final int error_field_required=0x7f0a001c;
-        public static final int error_incorrect_password=0x7f0a001b;
-        public static final int error_invalid_email=0x7f0a0019;
-        public static final int error_invalid_password=0x7f0a001a;
+        public static final int error_field_required=0x7f0a001d;
+        public static final int error_incorrect_password=0x7f0a001c;
+        public static final int error_invalid_email=0x7f0a001a;
+        public static final int error_invalid_password=0x7f0a001b;
         public static final int hello_world=0x7f0a000e;
-        public static final int login_progress_signing_in=0x7f0a0018;
+        public static final int login_progress_signing_in=0x7f0a0019;
         /**  Strings related to login 
          */
-        public static final int prompt_email=0x7f0a0013;
-        public static final int prompt_password=0x7f0a0014;
+        public static final int prompt_email=0x7f0a0014;
+        public static final int prompt_password=0x7f0a0015;
+        public static final int title_activity_consulta_notificacao=0x7f0a0012;
         public static final int title_activity_inicio=0x7f0a0011;
-        public static final int title_activity_login=0x7f0a0012;
+        public static final int title_activity_login=0x7f0a0013;
     }
     public static final class style {
         /** 
